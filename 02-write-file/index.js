8097ae1a2edfe1e2ff1090d2e6ab6330b9448cba
Into0 <Into0@users.noreply.github.com>
2024-01-18 01:10:47 +0300

    feat: add write file solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const writeStream = fs.createWriteStream(path.join(__dirname, 'text.txt'), 'utf8');
+
+console.log('Hello, Write Some Text\n');
+
+rl.on('line', (message) => {
+  if (message === 'exit') {
+    console.log('\nSee You Soon :)');
+    process.exit();
+  }
+  writeStream.write(`${message}\n`);
+});
+
+rl.on('SIGINT', () => {
+  console.log('\nSee You Soon :)');
+  process.exit();
+});
