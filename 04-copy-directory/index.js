07457362cf0b83e69252c9de094efd0106a14d6a
Into0 <Into0@users.noreply.github.com>
2024-01-19 00:57:27 +0300

    feat: add 'copy directory' solution

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const dirPath = fs.createReadStream(path.join(__dirname, 'files')).path;
+const copyPath = fs.createReadStream(path.join(__dirname, 'files-copy')).path;
+
+fs.mkdir(path.join(__dirname, 'files-copy'), () => {});
+
+fs.rm(copyPath, { recursive: true }, () => {
+  fs.mkdir(path.join(__dirname, 'files-copy'), () => {});
+
+  fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
+    const filter = files.filter(item => item.isFile()).map(item => item.name);
+    
+    filter.forEach((file) => {
+      fs.copyFile(`${dirPath}\\${file}`, `${copyPath}\\${file}`, () => {});
+    });
+  });
+});
