db38b5f933e1500ed37a0d0318089913f35daa36
Into0 <Into0@users.noreply.github.com>
2024-01-18 22:38:19 +0300

    feat: add 'files in folder' solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const dirPath = fs.createReadStream(path.join(__dirname, 'secret-folder')).path;
+
+fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
+  const filter = files.filter(item => item.isFile()).map(item => item.name);
+
+  filter.forEach((file) => {
+    fs.stat(dirPath + `\\${file}`, (err, stats) => {
+      const size = (stats.size / 1024).toFixed(3);
+      const ext = path.parse(`${file}`).ext.slice(1);
+      const name = path.parse(`${file}`).name;
+
+      console.log(`${name} - ${ext} - ${size}kb`);
+    });
+  });
+});
